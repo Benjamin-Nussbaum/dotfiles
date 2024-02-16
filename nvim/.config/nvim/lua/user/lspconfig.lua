9b2350fab3cac649d71d07d123ea5a105d6392d2
Benjamin Nussbaum <benjamin@benjaminnussbaum.xyz>
2024-02-15 21:11:38 -0600

    nvim: make pyright aware of venvs

--- nvim/.config/nvim/lua/user/lspconfig.lua
+++ nvim/.config/nvim/lua/user/lspconfig.lua
@@ -127,6 +127,27 @@ function M.config()
       require("neodev").setup({})
     end
 
+    if server == "pyright" then
+      opts.before_init = function(params, config)
+        local Path = require("plenary.path")
+        local venv = Path:new((config.root_dir:gsub("/", Path.path.sep)), ".venv")
+        if venv:joinpath("bin"):is_dir() then
+          config.settings.python.pythonPath = tostring(venv:joinpath("bin", "python"))
+        else
+          config.settings.python.pythonPath = tostring(venv:joinpath("Scripts", "python.exe"))
+        end
+      end
+    end
+
+    if server == "ruff_lsp" then
+      opts = vim.tbl_deep_extend("force", opts, {
+        on_attach = function(client, bufnr)
+          -- Disable hover in favor of Pyright
+          client.server_capabilities.hoverProvider = false
+        end,
+      })
+    end
+
     lspconfig[server].setup(opts)
   end
 end
